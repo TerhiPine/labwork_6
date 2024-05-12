9a048ea402a0090f1ab9697bc21069f87d5bdc6c
TerhiPine <TEP00006@laurea.fi>
2024-05-12 18:27:13 +0300

    imagegallery

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,20 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-
-const Menu: React.FC = () => {
-
+import {
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  } from "@ionic/react";
+  import { camera } from "ionicons/icons";
+  import PhotoGallery from "../components/PhotoGallery";
+  import { usePhotoGallery } from "../hooks/usePhotoGallery";
+  import { Link } from "react-router-dom";
+  
+  const Menu: React.FC = () => {
+    const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+  
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
-            </IonContent>
-        </IonPage>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color={"success"}>
+            <IonTitle>My Photo Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+  
+        <IonContent>
+          <PhotoGallery photos={photos} deletePhoto={deletePhoto} />
+  
+          <IonFab vertical="bottom" horizontal="end" slot="fixed">
+            <IonFabButton onClick={() => takePhoto()}>
+              <IonIcon icon={camera}></IonIcon>
+            </IonFabButton>
+          </IonFab>
+          <IonTitle>
+            <Link to="/"> Logout</Link>
+          </IonTitle>
+        </IonContent>
+      </IonPage>
     );
-};
-
-export default Menu;
+  };
+  
+  export default Menu;
